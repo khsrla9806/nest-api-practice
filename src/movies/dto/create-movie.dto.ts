419f9ab76b0f3235b6bb0eac32a6229383e31a9f
khsrla9806 <kimhunsope@naver.com>
2023-07-10 00:29:03 +0900

    refactor: 영화 장르는 선택적으로 사용할 수 있도록 @IsOptional 적용

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from "class-validator"
+import { IsNumber, IsOptional, IsString } from "class-validator"
 
 export class CreateMovieDto {
     @IsString()
@@ -7,6 +7,7 @@ export class CreateMovieDto {
     @IsNumber()
     readonly year: number
 
+    @IsOptional() // 있어도 되고, 없어도 되는 정보로 설정
     @IsString({each: true}) // 배열의 모든 요소가 모두 string 타입이어야 한다.
     readonly genres: string[]
 }
