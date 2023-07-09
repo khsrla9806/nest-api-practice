9ccb083cca297f6d0bb1bacbb604d408a1393c91
khsrla9806 <kimhunsope@naver.com>
2023-07-09 10:25:00 +0900

    feat: @Body, @Query 데코레이터 사용

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +7,21 @@ export class MoviesController {
         return '전체 영화 목록 조회';
     }
 
+    /** /:id 보다 아래 두면 search를 id로 인식한다. Nest 바보네 */
+    @Get('/search')
+    search(@Query('year') year: string) {
+        return `영화 검색: ${year}`;
+    }
+    
     @Get('/:id')
     getOne(@Param('id') id: string) {
         return `단일 영화 목록 조회: ${id}`;
     }
+    
 
     @Post()
-    createMovie() {
-        return '영화 생성'
+    createMovie(@Body() movieData) {
+        return movieData;
     }
 
     @Delete('/:id')
@@ -23,7 +30,10 @@ export class MoviesController {
     }
 
     @Patch('/:id')
-    updateMovie(@Param('id') id: string) {
-        return `영화 수정: ${id}`;
+    updateMovie(@Param('id') id: string, @Body() movieData) {
+        return {
+            updateMovieId: id,
+            ...movieData
+        }
     }
 }
